6c9badced6cb44111b092597e85fabce76b2f576
Kusuma-123643 <kusumakuppili24@gmail.com>
2025-03-15 18:36:48 +0530

    Add files via upload

---
+++ JS/Basics/C001.js
@@ -0,0 +1,9 @@
+// This is a single-line comment
+console.log("Hello, JavaScript!"); // This prints a message to the console
+
+/*
+ This is a multi-line comment.
+ It can span multiple lines.
+ Used for explaining complex logic.
+*/
+console.log("Learning JavaScript comments!");
